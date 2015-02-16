8a9b13b2ccd160391b6841f70e2219e515265f21
Burton Rast <burtonrast@gmail.com>
2015-02-16 15:35:40 -0600

    more devils, more comments, more details

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -14,7 +14,9 @@ app.controller('MainCtrl', function($scope) {
   $scope.tracks = [
     {
       'name': 'Ornery Little Darlings: Blueberry Hill (Dave Ruin Remix)',
-      'url': 'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/45982501&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true'
+      'url': 'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/45982501&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true',
+      'width': '100%',
+      'height': '300'
     }
   ];
 
